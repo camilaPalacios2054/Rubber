d30162f036fe167ac6b86fe1fcb229c21f0a8ab4
camilaPalacios2054 <c.palacios17@alumnos.santotomas.cl>
2023-11-29 23:38:17 -0300

    Initial commit

---
+++ app/src/main/java/com/example/rubber/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.rubber;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+import android.content.*;
+import androidx.fragment.app.Fragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText username;
+    EditText password;
+    Button loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        username=findViewById(R.id.Username);
+        password=findViewById(R.id.Password);
+        loginButton=findViewById(R.id.LoginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (username.getText().toString().equals("user")&&password.getText().toString().equals("123"))
+                {
+                    Toast.makeText(MainActivity.this,"Login Successful!",Toast.LENGTH_SHORT).show();
+                    Intent intent=new Intent(MainActivity.this,MapsActivity.class);
+                    startActivity(intent);
+
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this,"Login Failed!",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+
+    }
+}
