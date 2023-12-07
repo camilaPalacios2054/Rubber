49622961c7964c0851cd0f77fe76880baeae1d69
camilaPalacios2054 <c.palacios17@alumnos.santotomas.cl>
2023-12-07 14:40:02 -0300

    consulta a base de datos
    cargar datos funcional muestra base de datos completa

--- app/src/main/java/com/example/rubber/CrearUsuarioActivity.java
+++ app/src/main/java/com/example/rubber/CrearUsuarioActivity.java
@@ -1,33 +1,47 @@
 package com.example.rubber;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
-
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FieldPath;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class CrearUsuarioActivity extends AppCompatActivity {
 
     private EditText NombreyApellido,Email,Password;
-
+    private ListView datos;
     private FirebaseFirestore db;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_crear_usuario);
 
+        db=FirebaseFirestore.getInstance();
         NombreyApellido=findViewById(R.id.NombreyApellido);
         Email=findViewById(R.id.Email);
         Password=findViewById(R.id.PasswordRegistro);
+        datos=findViewById(R.id.datos);
 
-        db=FirebaseFirestore.getInstance();
+
     }
     public void enviarDatosFirestore(View view)
     {
@@ -55,5 +69,35 @@ public class CrearUsuarioActivity extends AppCompatActivity {
                     Toast.makeText(CrearUsuarioActivity.this,"Error al enviar los datos"+e.getMessage(),Toast.LENGTH_SHORT).show();
                 });
 
+    }
+    public void CargarLista(View view)
+    {CargarListaFirestore();}
+    public void CargarListaFirestore()
+    {
+        FirebaseFirestore db=FirebaseFirestore.getInstance();
+        db.collection("usuarios")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()){
+
+                            List<String>datoUsuarios=new ArrayList<>();
+
+                            for(QueryDocumentSnapshot document: task.getResult()){
+                                String linea="||"+document.getString("nombre")+"||"+
+                                        document.getString("email")+"||"+
+                                        document.getString("password");
+                                datoUsuarios.add(linea);
+                            }
+
+                            ArrayAdapter<String> adaptador=new ArrayAdapter<>(CrearUsuarioActivity.this, android.R.layout.simple_list_item_1,datoUsuarios);
+                            datos.setAdapter(adaptador);
+
+                        } else {
+                            Log.e("TAG", "Error al obtener los datos",task.getException());
+                        }
+                    }
+                });
     }
 }
