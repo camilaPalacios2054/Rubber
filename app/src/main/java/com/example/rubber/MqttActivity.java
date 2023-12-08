3016c38527605ef3e8218dfd1ed548e5eb6331ed
camilaPalacios2054 <c.palacios17@alumnos.santotomas.cl>
2023-12-08 00:09:23 -0300

    con errores

--- app/src/main/java/com/example/rubber/MqttActivity.java
+++ app/src/main/java/com/example/rubber/MqttActivity.java
@@ -2,13 +2,74 @@ package com.example.rubber;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
 
 public class MqttActivity extends AppCompatActivity {
+
+    String clienteID="";
+
+    //CONEXION AL SERVIDOR
+
+    //mqtt://santotomasproyect-mqtt:L8d9iJ3GBRveTeU9@santotomasproyect-mqtt.cloud.shiftr.io
+    static String MQTTHOST="tcp://santotomasproyect-mqtt.cloud.shiftr.io:1883";
+    static String MQTTUSER="santotomasproyect-mqtt";
+    static String MQTTPASS="L8d9iJ3GBRveTeU9";
+
+    MqttAndroidClient cliente;
+    MqttConnectOptions opciones;
+
+    TextView txtIdCliente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mqtt);
+
+        txtIdCliente=findViewById(R.id.txtIdCliente);
+
+        getNombreCliente();
+        connectBroker();
+    }
+    private void connectBroker()
+    {
+        this.cliente=new MqttAndroidClient(this.getApplicationContext(),MQTTHOST,this.clienteID);
+        this.opciones=new MqttConnectOptions();
+        this.opciones.setUserName(MQTTUSER);
+        this.opciones.setPassword(MQTTPASS.toCharArray());
+
+        try
+        {
+            IMqttToken token=this.cliente.connect(opciones);
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    Toast.makeText(getBaseContext(),"CONECTADO",Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    Toast.makeText(getBaseContext(),"CONEXIÓN FALLIDA",Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        catch (MqttException e){
+            e.printStackTrace();
+        }
+    }
+    private void getNombreCliente()
+    {
+        String manufacturer= Build.MANUFACTURER;
+        String modelName=Build.MODEL;
+        this.clienteID=manufacturer+" "+modelName;
+        txtIdCliente.setText(this.clienteID);
     }
 }
